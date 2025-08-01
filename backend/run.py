9f5b8c62bcd8d896949beaeb32143dffadddc1b5
Ajay <ajaynimmala18@gmail.com>
2025-08-01 22:39:41 +0530

    html is rendered

--- backend/run.py
+++ backend/run.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+template_dir = os.path.abspath('../frontend')
+
+app = Flask(__name__, template_folder=template_dir)
+
 
 @app.route('/')
 def home():
-    return '<h1>helloworld</h1>'
+    return render_template('home.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
