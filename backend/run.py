1374d163da3f79fda85e83c07bdb72b4f6433b4c
Ajay <ajaynimmala18@gmail.com>
2025-08-04 22:22:00 +0530

    simple register page added

--- backend/run.py
+++ backend/run.py
@@ -1,25 +1,19 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 import os
-
+from routes.user import user
 template_dir = os.path.abspath('../frontend')
 
 app = Flask(__name__, template_folder=template_dir)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
+app.register_blueprint(user)
 db = SQLAlchemy(app)
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
-@app.route('/register')
-def register():
-    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
