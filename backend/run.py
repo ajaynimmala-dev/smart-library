ba52d46a87c31265501255be7c87d4dc614a09f9
Ajay <ajaynimmala18@gmail.com>
2025-08-10 20:14:23 +0530

    User authentication added

--- backend/run.py
+++ backend/run.py
@@ -3,7 +3,6 @@ import os
 from flask import Flask, render_template
 
 from database.models import db
-from routes.auth import auth
 from routes.user_ui import user
 
 template_dir = os.path.abspath('../frontend')
@@ -12,7 +11,6 @@ app = Flask(__name__, template_folder=template_dir)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 app.register_blueprint(user)
-app.register_blueprint(auth, url_prefix="/auth")
 db.init_app(app)
 
 
