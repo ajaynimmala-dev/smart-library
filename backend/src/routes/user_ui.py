c84d885716b00b185c921ef52932e218f4e611af
Ajay <ajaynimmala18@gmail.com>
2025-08-12 22:32:05 +0530

    updated user dashboard

--- backend/src/routes/user_ui.py
+++ backend/src/routes/user_ui.py
@@ -1,10 +1,13 @@
-from flask import render_template,Blueprint,request,redirect
+from flask import render_template, Blueprint, request, redirect,url_for,flash
+
+from flask_login import current_user
 
 from database.models import User, db
 
-user = Blueprint('users',__name__)
+user = Blueprint('users', __name__)
 
-@user.route('/login',methods=["POST","GET"])
+
+@user.route('/login', methods=["POST", "GET"])
 def login():
     if request.method == "POST":
         if request.form:
@@ -13,18 +16,38 @@ def login():
             user_pass = form.get("password")
             check_email = User.query.filter_by(email=user_email).first()
             if check_email:
-                return 'login success' if check_email.password_hash == user_pass else 'password is incorrect'
-            return 'email is not exist'
+                if check_email.password_hash == user_pass:
+                    flash('successful login')
+                    return redirect(url_for('users.dashboard'))
+                else:
+                    flash('incorrect password')
+                    return redirect(url_for('users.login'))
+            else:
+                flash('email is not registered')
+                return redirect(url_for('users.register'))
     return render_template('login.html')
+
+
+@user.route('/dashboard',methods=["POST"])
+def dashboard():
+    data = request.get_json()
+    key = data.get("secret_key")
+    if key:
+        return render_template('dashboard.html')
+    flash('You need to login first')
+    return redirect(url_for('users.login'))
+
 
 @user.route('/register')
 def register():
     return render_template('register.html')
 
-@user.route('/create',methods=["POST"])
+
+@user.route('/create', methods=["POST"])
 def create():
     data = request.get_json()
-    user = User(user_id=data.get("rollnumber"),name=data.get("name"),email=data.get("email"),password_hash=data.get("password"))
+    user = User(user_id=data.get("rollnumber"), name=data.get("name"), email=data.get("email"),
+                password_hash=data.get("password"))
     db.session.add(user)
     db.session.commit()
     return 'success'
